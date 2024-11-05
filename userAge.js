9cc1b570be55f6c1ce68a7bcc63cdd0659879887
patricia145 <patriciavaldez0517@gmail.com>
2024-11-04 18:26:45 -0800

    initial commit

---
+++ userAge.js
@@ -0,0 +1,15 @@
+let age = 19;
+
+if (age < 5) {
+    console.log("You are a Toddler.");  
+} else if (age <= 12) {
+    console.log("You are a Child.");
+} else if (age <= 19) {
+    console.log("You are a Teenager.");
+} else if (age <= 35) {
+    console.log("You are a Adult.");
+} else if (age <= 60) {
+    console.log("You are a Middle-age.");
+} else {
+    console.log("You are a Senior.")
+}
